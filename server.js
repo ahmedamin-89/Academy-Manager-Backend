d400d07b9865d020ac26a2d771e5a14b5e467249
Ahmed Amin <ahmedamin100dev@gmail.com>
2024-07-09 03:40:15 +0300

    chore: Update dotenv dependency to version 16.4.5

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(async () => {
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
